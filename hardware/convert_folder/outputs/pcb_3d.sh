535baffcdf85d7465a646ba311e781a46cc21603
joaoclaudioeb <joaoclaudiobarcellos@gmail.com>
2026-07-11 22:46:05 -0300

    fix: fix a lot of connections between blocks. Many more to go...

--- hardware/convert_folder/outputs/pcb_3d.sh
+++ hardware/convert_folder/outputs/pcb_3d.sh
@@ -3,20 +3,22 @@ kicad-cli pcb render \
   --side top \
   --background transparent \
   --quality user \
+  --zoom 1.33 \
   -D render.floor=false \
   -D render.shadows=false \
   -D render.postprocess=true \
   -w 15000 -h 10000 \
   -o top_pcb.png \
-  ../sources/prototype.kicad_pcb
+  ../sources/flatsat_platform_2.kicad_pcb
 
 kicad-cli pcb render \
   --side bottom \
   --background transparent \
   --quality user \
+  --zoom 1.33 \
   -D render.floor=false \
   -D render.shadows=false \
   -D render.postprocess=true \
   -w 15000 -h 10000 \
   -o bottom_pcb.png \
-  ../sources/prototype.kicad_pcb
+  ../sources/flatsat_platform_2.kicad_pcb
